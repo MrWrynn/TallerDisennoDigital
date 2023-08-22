2b33d77d4ef8ac108ea631bc1c171193c983a431
eduardocv30 <jecruz@estudiantec.cr>
2023-08-21 19:46:55 -0600

    Arreglo de fallos

--- lab2/experimento_3/ContadorRegresivo_tb.sv
+++ lab2/experimento_3/ContadorRegresivo_tb.sv
@@ -1,13 +1,21 @@
+`timescale 10ns/1ns
 module ContadorRegresivo_tb();
 logic clk, reset;
-logic [5:0] count;
+logic [1:0] count;
 
-ContadorRegresivo #(6)(clk,reset,count);
+ContadorRegresivo #(2) test (clk,reset,count);
 
 initial begin
+clk = 1;
 reset=1;
-	forever begin
+#5;
+reset=0;
+#5;
+reset=1;
+#5;
+forever begin
 	clk=!clk;
+	#5;
 	end
 end
 
---
+++ lab2/experimento_3/ContadorRegresivo_tb.sv.bak
@@ -0,0 +1,18 @@
+module ContadorRegresivo_tb();
+logic clk, reset;
+logic [5:0] count;
+
+ContadorRegresivo #(6)(clk,reset,count);
+
+initial begin
+reset=1;
+	forever begin
+	clk=!clk;
+	end
+end
+
+
+
+endmodule
+
+
