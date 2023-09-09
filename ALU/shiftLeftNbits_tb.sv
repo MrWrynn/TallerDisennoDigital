87757ac5aad2ecd3052467b30dd789b699ad52ee
Ramsensei <ramsensei@estudiantec.cr>
2023-09-09 16:37:04 -0600

    shift listos

---
+++ ALU/shiftLeftNbits_tb.sv
@@ -0,0 +1,23 @@
+`timescale 10ns/1ns
+module shiftLeftNbits_tb();
+
+logic [3:0] a, b, shamt;
+
+shiftLeftNbits #(4) test (a, shamt, b);
+
+initial begin
+
+a=4'b0001;
+repeat (12) begin
+shamt=1;
+	repeat (6) begin
+		
+		#5;
+		assert (b === a<<shamt) else $error("Error en shamt %d y a %d", shamt, a);
+		shamt=shamt + 1;
+	end
+a=a + 1;
+end
+end
+
+endmodule
---
+++ ALU/shiftRightNbits.sv
@@ -0,0 +1,25 @@
+module shiftRightNbits #(N=4) (
+			input logic [N-1:0] aIn,
+			input logic [N-1:0] shamt,
+			output logic [N-1:0] bOut);
+			
+logic [N-1:0] temp = 0, zero = 0;
+logic [2*N-1:0] temp2 = 0;
+
+genvar i;
+generate
+	for (i = 0; i < N; i++)  begin : shift_creator
+		shift1bit sll (aIn[i], temp[i]);
+	end
+endgenerate	
+
+always @(*) begin
+if (shamt > N)
+	bOut = 0;
+else begin
+	temp2 = {zero, temp};
+	$display("temp: %b", temp2);
+	bOut = temp2[shamt +: N];
+end
+end
+endmodule
